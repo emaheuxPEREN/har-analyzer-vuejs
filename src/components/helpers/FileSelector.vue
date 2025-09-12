de6e0953590d2340289032a65371e609d4e1765e
Etienne Maheux <etienne.maheux@peren.gouv.fr>
2025-09-12 19:52:00 +0200

    Allow upload of multiples HAR files to be displayed together

--- src/components/helpers/FileSelector.vue
+++ src/components/helpers/FileSelector.vue
@@ -1,25 +1,37 @@
 <script setup>
-import {ref, useTemplateRef} from "vue";
+import {useTemplateRef} from "vue";
 import HarAnalyzer from "@/components/HarAnalyzer.vue";
 import {FileUpload} from "primevue";
 
-const entries = ref(0);
 const harAnalyzer = useTemplateRef('harAnalyzer');
 
+function readHarEntries(file) {
+  return new Promise(function(resolve, reject){
+    const reader = new FileReader();
+    reader.onload = function () {
+      const har = JSON.parse(this.result).log;
+      console.debug(`loaded ${har.entries.length} entries from ${file.name}`, har.creator);
+      resolve(har.entries);
+    };
+    reader.onerror = function(err) {
+      reject(err);
+    }
+    reader.readAsText(file);
+  });
+}
+
 function onHarSelection(event) {
-  const fileUp = event.files[0];
-  const reader = new FileReader();
-  reader.onload = function () {
-    const d = JSON.parse(reader.result);
-    harAnalyzer.value.setEntries(d.log.entries);
-  };
-  reader.readAsText(fileUp);
+  Promise.all(event.files.map(f => readHarEntries(f))).then(arrHarEntries => {
+    const allHarEntries = arrHarEntries.flat();
+    console.log(`loaded ${allHarEntries.length} entries in total`, allHarEntries.map(e => e._sha1Id));
+    harAnalyzer.value.setEntries(allHarEntries);
+  }).catch(console.error);
 }
 </script>
 
 <template>
   <div class="flex justify-end px-3">
-    <FileUpload mode="basic" @select="onHarSelection" customUpload auto
+    <FileUpload mode="basic" accept=".har" @select="onHarSelection" customUpload multiple auto
                 severity="secondary" class="p-button-outlined inline-flex h-8 text-sm "/>
   </div>
 
