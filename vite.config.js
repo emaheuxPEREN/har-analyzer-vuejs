db8191def19ae5466e17cc84dd5bc6160ff012dd
Etienne Maheux <etienne.maheux@peren.gouv.fr>
2025-09-23 10:57:41 +0200

    Remove custom rollup build options to be properly build with production

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import {fileURLToPath, URL} from 'node:url'
-import {defineConfig} from 'vite'
-import {resolve} from "path";
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import { resolve } from "path";
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '',
   plugins: [
     vue()
   ],
-  build: {
+  /*build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "HarAnalyzerVue",
@@ -24,10 +25,12 @@ export default defineConfig({
         exports: 'named',
         globals: {
           vue: 'Vue',
+          primevue: 'PrimeVue',
+          'primevue/config': 'PrimeVueConfig',
         },
       },
     },
-  },
+  },*/
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
